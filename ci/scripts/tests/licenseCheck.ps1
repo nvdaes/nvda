e6e5552891819878a62dcaee8e667709b1942048
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-02-26 17:46:25 +0100

    Test running licensecheck from ps1 invoking bat with cmd

--- ci/scripts/tests/licenseCheck.ps1
+++ ci/scripts/tests/licenseCheck.ps1
@@ -1,8 +1,3 @@
 $licenseOutput = (Resolve-Path .\testOutput\license\)
 $licenseOutput = "$licenseOutput\licenseCheckResults.md"
-.\runlicensecheck.bat | Out-File -FilePath "$licenseOutput" -Encoding utf8 -Append
-if ($LastExitCode -ne 0) {
-	"FAIL: License check. See $licenseOutput for more information." >> env:GITHUB_STEP_SUMMARY
-} else {
-	"PASS: License check." >> env:GITHUB_STEP_SUMMARY
-}
+cmd.exe /c "runlicensecheck.bat $licenseOutput"
