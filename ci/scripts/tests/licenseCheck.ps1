9324a865f126ea7e7701f4f0993c870fd3caf46a
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-02-26 07:57:54 +0100

    Try to save license check file

--- ci/scripts/tests/licenseCheck.ps1
+++ ci/scripts/tests/licenseCheck.ps1
@@ -1,6 +1,6 @@
 $licenseOutput = (Resolve-Path .\testOutput\license\)
 $licenseOutput = "$licenseOutput\licenseCheckResults.md"
-.\runlicensecheck.bat >> "$licenseOutput"
+.\runlicensecheck.bat | Out-File -FilePath "$licenseOutput" -Encoding utf8 -Append
 if ($LastExitCode -ne 0) {
 	"FAIL: License check. See $licenseOutput for more information." >> env:GITHUB_STEP_SUMMARY
 } else {
