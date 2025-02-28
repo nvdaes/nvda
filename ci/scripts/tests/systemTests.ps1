9f85acab018e6f5c85b456a9e0bc4c8a217f811b
Noelia Ruiz Martínez <nrm1977@gmail.com>
2025-02-28 18:52:12 +0100

    Don't compress system tests

--- ci/scripts/tests/systemTests.ps1
+++ ci/scripts/tests/systemTests.ps1
@@ -34,7 +34,6 @@ $includeTags = $tagsForTestArray | ForEach-Object {
 --variable verboseDebugLogging:"${verboseDebugLogging}" `
 @includeTags `
 # last line intentionally blank, allowing all lines to have line continuations.
-Compress-Archive -Path "$systemTestOutput\*" -DestinationPath "$testOutput\systemTestResult.zip"
 if($LastExitCode -ne 0) {
 	$MESSAGE = "FAIL: System tests (tags: ${tagsForTest}). See test results for more information."
 	echo "testFailExitCode=$LastExitCode" | Out-File -FilePath $Env:GITHUB_ENV -Encoding utf8 -Append
